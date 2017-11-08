3a7d3c2ebbe2beff7572c2416222fb5c91b8ddd4
morip <ggon45@gmail.com>
2017-11-08 15:56:53 +0900

    경로

--- src/main/java/org/mini/movie/web/MovieController.java
+++ src/main/java/org/mini/movie/web/MovieController.java
@@ -1,5 +1,13 @@
 package org.mini.movie.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.java.Log;
+
+@Log
+@Controller
+@RequestMapping("/movie/*")
 public class MovieController {
 
 }
